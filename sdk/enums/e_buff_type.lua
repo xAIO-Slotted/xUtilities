4fefe7f76a431f72684e0dc480e69411c0dbc982
ampx <ampxslotted@proton.me>
2023-03-27 06:14:21 +0200

    bik

---
+++ sdk/enums/e_buff_type.lua
@@ -0,0 +1,45 @@
+---@class e_buff_type
+---@field public internal number
+---@field public aura number
+---@field public combat_enchancer number
+---@field public combat_dehancer number
+---@field public spell_shield number
+---@field public stun number
+---@field public invisibility number
+---@field public silence number
+---@field public taunt number
+---@field public berserk number
+---@field public polymorph number
+---@field public slow number
+---@field public snare number
+---@field public damage number
+---@field public heal number
+---@field public haste number
+---@field public spell_immunity number
+---@field public physical_immunity number
+---@field public invulnerability number
+---@field public attack_speed_slow number
+---@field public near_sight number
+---@field public currency number
+---@field public fear number
+---@field public charm number
+---@field public poison number
+---@field public suppression number
+---@field public blind number
+---@field public counter number
+---@field public shred number
+---@field public flee number
+---@field public knockup number
+---@field public knockback number
+---@field public disarm number
+---@field public grounded number
+---@field public drowsy number
+---@field public asleep number
+---@field public obscured number
+---@field public clickproof_to_enemies number
+---@field public un_killable number
+---@field public unknown number
+local e_buff_type = {}
+
+---@type e_buff_type
+_G.e_buff_type = {}
--- xUtilities.lua
+++ xUtilities.lua
@@ -6,9 +6,11 @@ local navigation = menu.get_main_window():find_navigation(name + " " + version)
 
 -- Sections
 local safety_sec = navigation:add_section("Safety")
+
 local vision_sec = navigation:add_section("Vision")
 local misc_sec = navigation:add_section("Misc")
 local humanizer_sec = navigation:add_section("Humanizer")
+local drawings_sec = navigation:add_section("Drawings")
 
 -- Safety
 local anti_wall_flash = safety_sec:checkbox("stop flash fails", g_config:add_bool(true, "anti_wall_flash"))
@@ -34,89 +36,88 @@ end
 
 function modules.safeFlashing()
 
-  if g_input:is_key_pressed(safe_flash_key:get_value()) then
+    if g_input:is_key_pressed(safe_flash_key:get_value()) then
 
-    local spell_book = g_local:get_spell_book()
+        local spell_book = g_local:get_spell_book()
 
-    local slot = e_spell_slot.d
+        local slot = e_spell_slot.d
 
-    if not spell_book:get_spell_slot(e_spell_slot.d):get_name() == "SummonerFlash" then
-      slot = e_spell_slot.f
-    end
-    
-    local fail = g_navgrid:is_wall(g_input:get_cursor_position_game())  or g_navgrid:is_building(g_input:get_cursor_position_game())
-      
-    if spell_book:get_spell_slot(slot):get_name() == "SummonerFlash" then
-      
-      local dis = math.min(400, g_local.position:dist_to(g_input:get_cursor_position_game()))
-      if anti_short_flash:get_value() and not fail and dis <= 399 then
-        g_input:cast_spell(slot, g_local.position:extend(g_local.position:extend(g_input:get_cursor_position_game(), dis), 400))
-        return
-      end
-
-      if anti_wall_flash:get_value() and fail then
-
-        local inWall = true
-        local impossible = false
-        local closest = math.huge
-        local walkPos = vec3:new(0,0,0)
-        local flashPos = vec3:new(0,0,0)
-        local cursor = g_input:get_cursor_position_game()
-
-        for i = 40, 400, 40 do
-
-          local points = circlePoints(cursor, i, 360)
-
-          for _, point in ipairs(points) do
-              if not g_navgrid:is_wall(point) and not g_navgrid:is_building(point) then
-                  impossible = true
-                  local pointDist = g_local.position:dist_to(point)
-
-                  if zero(walkPos) and cursor:dist_to(point) < closest then
-                      closest = cursor:dist_to(point)
-                      inWall = false
-                      flashPos = point
-                  end
-
-                  if pointDist < dis and (zero(walkPos) or g_local.position:dist_to(walkPos) > g_local.position:dist_to(point)) then
-                      inWall = true
-                      walkPos = point
-                  end
-              end
-            end
-            if impossible then break end
-          end
-
-          if inWall and not zero(walkPos) then
-            g_input:issue_order_move(walkPos)
-            return
-
-          elseif not zero(flashPos) then
-            g_input:cast_spell(slot, flashPos)
-            return
-
-          end
+        if not spell_book:get_spell_slot(e_spell_slot.d):get_name() == "SummonerFlash" then
+            slot = e_spell_slot.f
         end
-      else
-        print("xUtils: Unable to safe-flash without flash.")
-        return
-      end
-  end
+
+        local fail = g_navgrid:is_wall(g_input:get_cursor_position_game())  or g_navgrid:is_building(g_input:get_cursor_position_game())
+
+        if spell_book:get_spell_slot(slot):get_name() == "SummonerFlash" then
+
+            local dis = math.min(400, g_local.position:dist_to(g_input:get_cursor_position_game()))
+            if anti_short_flash:get_value() and not fail and dis <= 399 then
+                g_input:cast_spell(slot, g_local.position:extend(g_local.position:extend(g_input:get_cursor_position_game(), dis), 400))
+                return
+            end
+
+            if anti_wall_flash:get_value() and fail then
+
+                local inWall = true
+                local impossible = false
+                local closest = math.huge
+                local walkPos = vec3:new(0,0,0)
+                local flashPos = vec3:new(0,0,0)
+                local cursor = g_input:get_cursor_position_game()
+
+                for i = 40, 400, 40 do
+
+                    local points = circlePoints(cursor, i, 360)
+
+                    for _, point in ipairs(points) do
+                        if not g_navgrid:is_wall(point) and not g_navgrid:is_building(point) then
+                            impossible = true
+                            local pointDist = g_local.position:dist_to(point)
+
+                            if zero(walkPos) and cursor:dist_to(point) < closest then
+                                closest = cursor:dist_to(point)
+                                inWall = false
+                                flashPos = point
+                            end
+
+                            if pointDist < dis and (zero(walkPos) or g_local.position:dist_to(walkPos) > g_local.position:dist_to(point)) then
+                                inWall = true
+                                walkPos = point
+                            end
+                        end
+                    end
+                    if impossible then break end
+                end
+
+                if inWall and not zero(walkPos) then
+                    g_input:issue_order_move(walkPos)
+                    return
+
+                elseif not zero(flashPos) then
+                    g_input:cast_spell(slot, flashPos)
+                    return
+
+                end
+            end
+        else
+            print("xUtils: Unable to safe-flash without flash.")
+            return
+        end
+    end
 end
 
 function modules.lantern()
 
-  if anti_lantern:get_value() then
-		local control_ward_slot = e_spell_slot.item4
-		objects = features.entity_list:ally_uncategorized() -- TODO: change back to: enemy_uncategorized()
-		for _, minion in ipairs(objects) do
-			if minion.is_enemy and minion:dist_to(g_local.position) <= 600 and minion.object_name == "ThreshLantern" then
-				g_input:cast_spell(control_ward_slot, vec3:new(minion.position.x, minion.position.y, minion.position.z))
-        break
-			end
-		end
-	end
-
+    if anti_lantern:get_value() then
+        local control_ward_slot = e_spell_slot.item4
+        objects = features.entity_list:ally_uncategorized() -- TODO: change back to: enemy_uncategorized()
+        for _, minion in ipairs(objects) do
+            if minion.is_enemy and minion:dist_to(g_local.position) <= 600 and minion.object_name == "ThreshLantern" then
+                g_input:cast_spell(control_ward_slot, vec3:new(minion.position.x, minion.position.y, minion.position.z))
+                break
+            end
+        end
+    end
 end
 
 cheat.on("features.run", function()
